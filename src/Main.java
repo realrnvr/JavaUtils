78ee61754004fd9494f776ecc35bfea9ebc43b77
realrnvr <realrnvr@gmail.com>
2025-03-25 01:37:24 +0530

    custom-hashmap-implementation tests

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,26 @@
+import CustomUtil.HashMap.CustomHashMap;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        CustomHashMap<Integer, Integer> ref = new CustomHashMap<>();
 
+        ref.put(1, 2);
+        ref.put(2, 4);
+        ref.put(3, 6);
+        ref.put(4, 8);
+
+        System.out.println(ref.get(1));
+        System.out.println(ref.get(2));
+        System.out.println(ref.get(3));
+        System.out.println(ref.get(4));
+
+        ref.remove(3);
+
+        System.out.println(ref.get(3));
+
+        System.out.println(ref);
     }
 }
