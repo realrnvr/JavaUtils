773eeb047aff55994d0ef0e18538691894243fa8
realrnvr <realrnvr@gmail.com>
2025-03-25 15:57:14 +0530

    com

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,12 @@
+import CustomUtil.ArrayList.CustomArrayList;
 import CustomUtil.HashMap.CustomHashMap;
+import CustomUtil.HashSet.CustomHashSet;
 
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 7, 5, 3, 7};
-        CustomHashMap<Integer, Integer> ref = new CustomHashMap<>();
-
-        for (int j : arr) {
-            ref.put(j, ref.getOrDefault(j, 0) + 1);
-        }
-
-        System.out.println(ref.containsKey(10));
-        System.out.println(ref.containsKey(1));
-        System.out.println(ref.containsValue(2));
-
-        System.out.println(ref);
+//        CustomArrayList<Integer> list = new CustomArrayList<>();
+//        CustomHashMap<Integer, Integer> map = new CustomHashMap<>();
+//        CustomHashSet<Integer> set = new CustomHashSet<>();
     }
 }
